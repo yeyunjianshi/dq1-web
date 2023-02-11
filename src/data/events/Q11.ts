90a7543ff083a536e29a9928d6fb04c59fccd82d
刚出炉的小肥腿 <547305181@qq.com>
2023-02-11 22:03:57 +0800

    fix(gameplay): 修复卖钥匙和卖香水事件收费后物品没增加的问题

--- src/data/events/Q11.ts
+++ src/data/events/Q11.ts
@@ -8,11 +8,16 @@ task(async (event) => {
     textArgs: [['{money}', money]],
   })
   while (select) {
+    if (checkInventoryIsFull()) {
+      await talk('*', 'InventoryIsFull')
+      break
+    }
     if (money > hero().gold) {
       await talk('*', 'NotEnoughMoney')
       break
     }
     hero().gold -= money
+    addItems(50)
     select = await talk('*', 'Q11_1', false, true)
   }
   await talk('*', 'Q11_N')
